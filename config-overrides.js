b77d244e895a21f066946f8e110b13ea99250baa
Infatutaion <971806774@qq.com>
2019-07-25 11:26:17 +0800

    解决冲突

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const {
-    override,
-    fixBabelImports
-} = require('customize-cra');
-module.exports = function override(config, env) {
-    // do stuff with the webpack config...
-    return config;
-};
-
-module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd-mobile',
-        style: 'css',
-    }),
-=======
 const {
     override,
     fixBabelImports
@@ -28,5 +12,4 @@ module.exports = override(
         libraryName: 'antd-mobile',
         style: 'css',
     })
->>>>>>> f879e2e6609ae8fc0d04ab0dcb2a4da9fb24a15a
 );
